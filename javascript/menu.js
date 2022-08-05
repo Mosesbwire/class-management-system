5f07e2982f3dedb115ea88f2432d049888b338e3
musa1992 <mosesbwire91@gmail.com>
2022-08-05 12:47:51 +0300

    add and style assingment page

--- javascript/menu.js
+++ javascript/menu.js
@@ -1,23 +1,19 @@
 
 const menuIcon = document.getElementById('menu-toggle');
 
-
 menuIcon.addEventListener('click', toggleMobileMenu)
-
 
 function toggleMobileMenu(){
     const menu = document.getElementById('menu');
     const closeMenuBtn = document.getElementById('close-menu')
     const width = menu.clientWidth;
     
-
     menu.style.visibility = "visible"
     menu.style.left = "0px"
 
     closeMenuBtn.addEventListener('click', ()=>{
         let offset = width + 100
         menu.style.left = `-${offset}px`
-
     })
 }
 
