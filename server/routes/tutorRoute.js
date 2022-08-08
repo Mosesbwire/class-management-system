68bfc6385829b11a1c0421cac2b3bfb228fa4065
musa1992 <mosesdev92@gmail.com>
2022-08-08 19:52:40 +0300

    add route to get sign up form, add post route to create tutor

--- server/routes/tutorRoute.js
+++ server/routes/tutorRoute.js
@@ -1,6 +1,14 @@
 const express = require('express')
 
+const {signUp,
+       create
+
+    } = require('../controllers/tutorController')
+
 const tutorRouter = express.Router()
+
+tutorRouter.get('/sign-up', signUp)
+tutorRouter.post('/sign-up', create)
 
 
 module.exports = tutorRouter
