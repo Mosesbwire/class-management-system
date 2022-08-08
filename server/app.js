e123bb2c30340bdf9d5686eb778005679f46a78c
musa1992 <mosesdev92@gmail.com>
2022-08-08 15:48:29 +0300

    configure mongo db connection

--- server/app.js
+++ server/app.js
@@ -2,12 +2,23 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+dotenv.config()
+
+const mongoDB = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@class.otxyqq2.mongodb.net/?retryWrites=true&w=majority`
+
+mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'MongoDB CONNECTION ERROR'))
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
