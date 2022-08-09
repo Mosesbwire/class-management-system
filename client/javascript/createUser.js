a634d157b6a59ece915774e258c0a6cb843048fe
musa1992 <mosesdev92@gmail.com>
2022-08-09 11:09:38 +0300

    add fetch method to send user sign up data to express server

---
+++ client/javascript/createUser.js
@@ -0,0 +1,32 @@
+
+const form = document.getElementById('sign-up-tutor')
+const inputArray = Array.from(form.getElementsByTagName('input'))
+const data = {}
+
+
+form.addEventListener('submit', async (event)=>{
+    event.preventDefault()
+    inputArray.forEach(e=>{
+        if(e.name !== 'confirm-password'){
+            data[e.name] = e.value
+        }
+    })
+    
+    const response = await fetch('http://localhost:3000/tutor/sign-up', {
+        method: 'POST',
+        body: JSON.stringify(data),
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        }
+    })
+
+    console.log(response)
+
+})
+
+
+
+
+
+
+
