328ecd91e2a62da4f83319f528e8d6139502f637
musa1992 <mosesdev92@gmail.com>
2022-08-16 18:05:46 +0300

    create class routes

---
+++ server/routes/classRoute.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const classRouter = express.Router()
+const { ensureAuthenticated} = require('../config/auth')
+
+const { create,
+        getAllClasses,
+        getClass
+} = require('../controllers/classController')
+
+
+classRouter.post('/create', ensureAuthenticated, create)
+classRouter.get('/classes',ensureAuthenticated, getAllClasses)
+classRouter.get('/:id',ensureAuthenticated.getClass)
+
+
+
+module.exports = classRouter
