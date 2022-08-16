7f9b008a44e8bd73338e1d3179e9b16e15786bc2
musa1992 <mosesdev92@gmail.com>
2022-08-16 17:59:25 +0300

    add create, getAllClasses and getClass functions in class controller

---
+++ server/controllers/classController.js
@@ -0,0 +1,48 @@
+const Class = require('../models/classModel')
+// const Tutor = require('../models/tutorModel')
+
+
+function create(req,res,next){
+    const course = new Class({
+        tutor: req.user.id,
+        name: req.body.name,
+        totalHours: req.body.hours,
+        totalNummberOfTopics: req.body.topics,
+
+    })
+
+    course.save(function(err){
+        if(err){ return next(err)}
+
+        res.status(201).json(course)
+    })
+}
+
+function getAllClasses(req,res,next){
+    Class.find({tutor: req.user.id},(err,classes)=>{
+        if(err){ return next(err)}
+
+        if(classes === null){
+            return res.status(204).json({message: 'You currently have no classes'})
+        }
+
+        res.status(200).json(classes)
+
+    })
+    
+}
+
+async function getClass(req,res,next){
+    const classId = req.params.id
+    Class.findById(classId, (err,course)=>{
+        if(err){ return next(err)}
+
+        res.status(200).json(course)
+    })
+}
+
+module.exports = {
+    create,
+    getAllClasses,
+    getClass
+}
