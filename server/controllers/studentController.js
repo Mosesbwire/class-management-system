1cfdc0956595b2fcf29e16d72d5ec1c3d6696c94
musa1992 <mosesdev92@gmail.com>
2022-08-17 23:33:47 +0300

    add create, login,logout functions to student controller file

---
+++ server/controllers/studentController.js
@@ -0,0 +1,51 @@
+const Student = require('../models/studentModel')
+const passport = require('passport')
+
+
+async function create(req,res,next){
+    const student = new Student({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email
+    })
+
+    student.password = await student.generateHash(req.body.password)
+
+    student.save(function(err){
+        if (err){ return next(err)}
+
+        req.logIn(student,function(err){
+            if(err){ return next(err)}
+
+            res.status(201).json(student)
+        })
+    })
+}
+
+function login(req,res,next){
+    passport.authenticate('local', {}, (err,user,info)=>{
+
+        if(!user){
+            return res.status(401).json(info)
+        }
+        req.logIn(user,function(err){
+            if(err) {return next(err)}
+            return res.json(req.user)
+        })
+    })(req,res,next)
+}
+
+function logout(req,res,next){
+    req.logOut(function(err){
+        if(err){ return next(err)}
+
+        res.status(200).json({message: 'Logged out'})
+    })
+}
+
+
+module.exports = {
+    create,
+    login,
+    logout
+}
