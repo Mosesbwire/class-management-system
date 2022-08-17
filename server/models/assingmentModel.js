0dc16b46ccafefa539d0bea000dbcfba48a1e549
musa1992 <mosesdev92@gmail.com>
2022-08-17 15:51:18 +0300

    add assingment model

---
+++ server/models/assingmentModel.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const AssingmentSchema = new Schema({
+    name: {type: String, required: true},
+    dueDate: {type: Date, required: true },
+    file: {type: Buffer, required: true},
+    tutor: {type: Schema.Types.ObjectId, required: true},
+    course: {type: Schema.Types.ObjectId, required: true},
+    score: {type: Number, required: true}
+})
+
+
+module.exports = mongoose.model('Assingment', AssingmentSchema)
