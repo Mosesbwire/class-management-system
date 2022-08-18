2b136eac029a51db7279ab9baeb1ac5af1fca9f9
musa1992 <mosesdev92@gmail.com>
2022-08-18 19:34:42 +0300

    change the file type from binary to string

--- server/models/assingmentModel.js
+++ server/models/assingmentModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const AssingmentSchema = new Schema({
     name: {type: String, required: true},
     dueDate: {type: Date, required: true },
-    file: {type: Buffer, required: true},
+    file: {type: String, required: true},
     tutor: {type: Schema.Types.ObjectId, required: true},
     course: {type: Schema.Types.ObjectId, required: true},
     score: {type: Number, required: true}
