7c072745324227512628afd479ca40c3b6a62c48
musa1992 <mosesdev92@gmail.com>
2022-08-21 15:37:19 +0300

    add passport local-student strategy to login students

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,33 +2,56 @@ const LocalStrategy = require('passport-local')
 const bcrypt = require('bcrypt')
 
 const Tutor = require('../models/tutorModel')
+const Student = require('../models/studentModel')
+
+
 
 module.exports = function(passport){
 
 passport.use('local',new LocalStrategy(function verify(username, password,cb){
+
   
-  Tutor.findOne({e_mail_address: username},function(err,tutor){
-      if(err) { return cb(err) }
-      if(!tutor){ return cb(null,false,{message: 'Email provided does not exist'}) } 
+  Tutor.findOne({email: username}, function(err,tutor){
+    if(err){return cb(err)}
+    
+    if(!tutor){
+      return cb(null,false, {message: 'The email provided does not exist'})
+    }
 
-
-      bcrypt.compare(password, tutor.password, (err, isMatch)=>{
-          if(err) { return cb(err)}
-
-          if(!isMatch){ return cb(null, false, {message: 'Incorrect password'}) }
-
-          return cb(null, tutor)
-      })
-      
+    bcrypt.compare(password, tutor.password, (err, isMatch)=>{
+      if(err) { return cb(err)}
+  
+      if(!isMatch){ return cb(null, false, {message: 'Incorrect password'}) }
+  
+      return cb(null, tutor)
+    })
   })
+}))
+passport.use('local-student',new LocalStrategy(function verify(username, password,cb){
 
+  
+  Student.findOne({email: username}, function(err,student){
+    if(err){return cb(err)}
+    
+    if(!student){
+      return cb(null,false, {message: 'The email provided does not exist'})
+    }
+
+    bcrypt.compare(password, student.password, (err, isMatch)=>{
+      if(err) { return cb(err)}
+  
+      if(!isMatch){ return cb(null, false, {message: 'Incorrect password'}) }
+  
+      return cb(null, student)
+    })
+  })
 }))
 
 
 passport.serializeUser(function(user, cb) {
    
     process.nextTick(function() {
-      cb(null, { id: user._id, username: user.e_mail_address });
+      cb(null, { id: user._id, username: user.email });
     });
   });
   
